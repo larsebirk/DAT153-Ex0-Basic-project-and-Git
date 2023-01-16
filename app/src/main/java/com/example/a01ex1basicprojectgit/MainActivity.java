d2a94c8d9cf59195e4989e5ce342dd346b9e9d59
h593291 <593291@stud.hvl.no>
2023-01-16 15:29:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a01ex1basicprojectgit/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a01ex1basicprojectgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView text = findViewById(R.id.textClicked);
+        text.setText("You clicked the button!");
+    }
+}
